63cd3fe08795a6800c7620018f9006b6c21feaf8
Nehasingh231 <nehakumari14072001@gmail.com>
2024-06-03 00:42:52 +0530

    App

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",
-{id: "heading",XYZ: "abc"},
-"Hello people from React"
+// React.createElement
+
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "hey neha"
+// );
+
+
+// JSX 
+
+const heading = (<h1 id="heading"
+ className="head">
+ Namaste Everyone
+ </h1>
 );
 
-console.log(heading);
+const element = <span>I am react element</span>
 
-const root =  ReactDOM.createRoot(document.getElementById("root"));
+const title = (
+    <h4>Hey i am just a variable
+    {element}
+    </h4>
+);
 
-root.render(heading);
+// React Functional Copmponent
+
+const Title = () => {
+   return <h1 className="head">
+  Namaste Everyone...
+ </h1>
+}
+ const number = 1000;
+// Component Composition - component inside component
+
+const HeadingComponent = ()=>(
+    <div id = "container">
+    <h2>{100 + 300}</h2>
+   <h3>{console.log ("JSX is amazing")}</h3> 
+  <Title /> 
+  <Title></Title>
+  {Title()} 
+    {number}
+    {title}
+     <h1>Namaste React Functional Component</h1>
+    </div>
+);
+
+
+// root.render(heading);
+// root.render(heading);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />)
